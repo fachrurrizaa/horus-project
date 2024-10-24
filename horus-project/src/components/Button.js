de705e6b6a991da5c6f67bd765c52d9dec4a7710
21.11.4318_Muhammad Fachrur Riza <72857515+fachrurrizaa@users.noreply.github.com>
2024-10-24 20:54:04 +0700

    Delete horus-project directory

--- horus-project/src/components/Button.js
+++
@@ -1,6 +0,0 @@
-export default function Button({ content, className, click, type }) {
-    return (
-      <button className={`btn outline-none border-none rounded-full ${className}`} onClick={click} type={type}>{content}</button>
-    )
-  }
-  
